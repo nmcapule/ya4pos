189e4432c76e0f360cbef197898fe617acd83e18
Nathaniel M. Capule <llss.s.sls.lll@gmail.com>
2023-02-24 11:14:07 +0000

    feat: Basic update to Review screen.

--- client/screens/pos/Review.tsx
+++ client/screens/pos/Review.tsx
@@ -1,15 +1,50 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { useEffect } from "react";
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { DataTable, Text } from "react-native-paper";
+import { WarehouseService } from "../../services/warehouse";
 
 import { RootStackParamList } from "../navigation";
 
 export default function ReviewScreen({
     route,
 }: NativeStackScreenProps<RootStackParamList, "POS">) {
+    const { orders }: { orders: Record<string, number> } = route.params!;
+
+    useEffect(() => {
+        const service = new WarehouseService();
+        (async () => {
+            const stocks = await service.Stocks(
+                HARDCODED_WAREHOUSE_ID,
+                new URLSearchParams({
+                    filter: `is_sellable=true`,
+                    expand: `item_id.tags`,
+                })
+            );
+            const tags = stocks
+                .map((stock) => stock.expand?.item_id.expand?.tags as Tag[])
+                .flat()
+                .filter((tag) => Boolean(tag))
+                .reduce(
+                    (tags, tag) => ({ ...tags, [tag.id!]: tag }),
+                    {} as Record<string, Tag>
+                );
+
+            setStocks(stocks);
+            setTags(Object.values(tags));
+        })();
+    }, []);
+
     return (
         <View>
-            <Text>{JSON.stringify(route.params, null, 2)}</Text>
+            <Text>{JSON.stringify(orders, null, 2)}</Text>
+            <DataTable>
+                <DataTable.Header>
+                    <DataTable.Title>Item</DataTable.Title>
+                    <DataTable.Title numeric>Quantity</DataTable.Title>
+                    <DataTable.Title numeric>Total Price</DataTable.Title>
+                </DataTable.Header>
+            </DataTable>
         </View>
     );
 }
