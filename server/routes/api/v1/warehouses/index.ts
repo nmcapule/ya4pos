1735c85c2b94c6d3b73f999bbe6ade3127a23cda
Nathaniel M. Capule <llss.s.sls.lll@gmail.com>
2023-03-19 05:15:34 +0000

    Cleanup.

--- server/routes/api/v1/warehouses/index.ts
+++
@@ -1,8 +0,0 @@
-import type { Handlers } from "$fresh/server.ts";
-import type PocketBase from "pocketbase";
-import { PocketBaseModel } from "@/models/index.ts";
-import { CRUDFactory } from "@/utils/pocketbase.ts";
-
-export const handler: Handlers<unknown, { pb: PocketBase }> = {
-    GET: CRUDFactory.List(PocketBaseModel.WAREHOUSES),
-};
--- server/routes/sandbox/index.tsx
+++
@@ -1,6 +0,0 @@
-import type { PageProps } from "$fresh/server.ts";
-import Sandbox from "@/islands/Sandbox.tsx";
-
-export default function Index() {
-    return <Sandbox />;
-}
